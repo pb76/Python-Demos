a02f79a93571bfd058e66cc137a58eed27b348fa
mcwolfe <mcwolfemeida@gmail.com>
2013-04-15 11:56:11 +0200

    Kort om filer
    Kort beskrivning av hur man arbetar med filer

---
+++ filer.py
@@ -0,0 +1,20 @@
+'''
+Öppnar en fil. Kan ges extra parametrar
+
+t.ex
+open("lista.txt","r+") öppnar för läsa och skriva
+r = läsa <- default-värde
+r+ = både läsa och skriva
+w = bara skriva (skriver över redan existerande fil)
+a = lägg till i slutet på existerande fil
+'''
+minLista = open("lista.txt")
+
+print("skriv ut filens innehåll")
+for rad in minLista:
+	print(rad)
+
+print("skriver ut första posten igen")
+#Nollställer listans pekare
+minLista.seek(0)
+print(minLista.readline())
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 #Demofiler
 
 Här lägger vi de demofiler i Python som vi skapar.
+
+## Förslag på mer demo-punkter
+- Filhantering
+- Json 
+- XML
+- TKinter
+- Internet-tjänster
